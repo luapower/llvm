f4008a068d0309186dac3d623d9617c41aa56a7c
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-06-06 14:55:54 +0300

    unimportant

--- csrc/llvm/build-mingw.cmd
+++ csrc/llvm/build-mingw.cmd
@@ -5,7 +5,7 @@ if [%1] == [] echo don't call this directly. & exit
 
 cat AddLLVM.cmake.patch | patch -N llvm.src/cmake/modules/AddLLVM.cmake
 
-REM remove MSYS from PATH because cmake is dumb.
+REM remove MSYS from PATH because CMAKE is dumb.
 set $line=%path%
 for %%a in ("%path:;=";"%") do (
     echo %%a | find /i "msys" >nul && echo Removed %%a from PATH to please CMAKE. || set $newpath=!$newpath!;%%a
@@ -27,4 +27,3 @@ cd clang.build.mingw%1
 cmake -DLLVM_CONFIG=../install.mingw%1/bin/llvm-config -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ../clang.src
 cmake --build .
 cmake -DCMAKE_INSTALL_PREFIX=../install.mingw%1 -P cmake_install.cmake
-
